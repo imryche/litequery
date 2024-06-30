6fdd353eed918b4d75c127d72ac97243395db30f
ryche <imryche13@gmail.com>
2024-06-30 17:49:23 +0200

    configure setup and teardown process for tests

--- tests/test_litequery.py
+++ tests/test_litequery.py
@@ -1,11 +1,34 @@
+import os
 import pytest
+import aiosqlite
 import litequery
+import pytest_asyncio
+
+DATABASE_PATH = "test.db"
+QUERIES_PATH = "queries.sql"
+
+
+@pytest_asyncio.fixture
+async def setup_database():
+    async with aiosqlite.connect(DATABASE_PATH) as conn:
+        await conn.execute(
+            "create table users (id integer primary key autoincrement, name text)"
+        )
+        await conn.commit()
+    yield
+    os.remove(DATABASE_PATH)
+
+
+@pytest_asyncio.fixture
+async def lq(setup_database):
+    lq = litequery.create(DATABASE_PATH, QUERIES_PATH)
+    await lq.connect()
+    yield lq
+    await lq.disconnect()
 
 
 @pytest.mark.asyncio
-async def test_queries():
-    lq = litequery.create("users.db", "queries.sql")
-    await lq.connect()
+async def test_queries(lq):
     print(await lq.users_delete_all())
     print(await lq.users_insert(name="kocia"))
     print(await lq.users_insert(name="kot"))
@@ -13,4 +36,3 @@ async def test_queries():
     print(await lq.users_insert(name="bunchik"))
     print(await lq.users_all())
     print(await lq.users_first())
-    await lq.disconnect()
