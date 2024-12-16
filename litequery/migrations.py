a28eb46d13ada1ff8e274998ef43dc77471dbc47
ryche <imryche13@gmail.com>
2024-12-16 21:15:41 +0100

    extract schema generation into a separate function

--- litequery/migrations.py
+++ litequery/migrations.py
@@ -8,7 +8,7 @@ import textwrap
 def migrate(db_path, migrations_dir):
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
-    c = conn.cursor()
+    cur = conn.cursor()
 
     filenames = sort_migration_filenames(
         os.path.basename(p) for p in glob.glob(f"{migrations_dir}/*.sql")
@@ -20,8 +20,8 @@ def migrate(db_path, migrations_dir):
             run_at text not null default current_timestamp
         );
     """
-    c.execute(textwrap.dedent(query).strip())
-    migrations = c.execute("select * from migrations order by run_at asc").fetchall()
+    cur.execute(textwrap.dedent(query).strip())
+    migrations = cur.execute("select * from migrations order by run_at asc").fetchall()
     migrations = {m["filename"] for m in migrations}
     unapplied = sort_migration_filenames(set(filenames) - migrations)
 
@@ -33,22 +33,26 @@ def migrate(db_path, migrations_dir):
     for file in unapplied:
         print(f"- {file}")
         with open(f"{migrations_dir}/{file}") as f:
-            c.executescript(f.read())
+            cur.executescript(f.read())
 
-        c.execute(
+        cur.execute(
             "insert into migrations (filename) values (:filename)",
             {"filename": file},
         )
         conn.commit()
 
+    generate_schema(cur, db_path)
+
+    conn.close()
+
+
+def generate_schema(cur, db_path):
     with open(os.path.join(os.path.dirname(db_path), "schema.sql"), "w") as f:
-        statements = c.execute(
+        statements = cur.execute(
             "select sql from sqlite_master where sql is not null"
         ).fetchall()
         for (statement,) in statements:
             f.write(f"{statement};\n")
-
-    conn.close()
 
 
 def sort_migration_filenames(filenames):
